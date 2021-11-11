72bbc9b10322c000b5d6c8aa80baca6584c123b6
scatowl <scatowl@qq.com>
2021-11-11 23:16:22 +0800

    beautify

--- webhook_parser.py
+++ webhook_parser.py
@@ -1,12 +1,16 @@
 import re
 import json
 import time
+import requests
+import os
 
 with open('epic.log', 'r') as log:
     log = log.readlines()
 # print(log)
 hook_content = []
 emoji = {'WARN': ':warning: ', 'INFO': ':information_source: '}
+index = []
+webhook_url = os.getenv('Discord_Webhook')
 for i in log[3:]:
     ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
     result = ansi_escape.sub('', i).strip().split('|')
@@ -18,6 +22,9 @@ for i in log[3:]:
     # print(result)
     hook_content.append(result)
 # print(hook_content)
+for a in hook_content:
+    if 'out of Epic Games' in a['value']:
+        index.append(hook_content.index(a))
 discord_hook = {
     "username": "Epic Log",
     "avatar_url": "https://i.imgur.com/4M34hi2.png",
@@ -36,7 +43,11 @@ discord_hook = {
         },
     ]
 }
-discord_hook['embeds'][0]['fields'] = hook_content
-# print(discord_hook)
-with open('webhook.json','w') as webhook:
-    json.dump(discord_hook,webhook)
+for b in index:
+    num = index.index(b)
+    prev = 0 if num == 0 else index[num - 1]
+    discord_hook['embeds'][0]['fields'] = hook_content[prev:b + 1]
+    # print(discord_hook['embeds'][0]['fields'])
+    with open('webhook.json', 'w') as webhook:
+        json.dump(discord_hook, webhook)
+    requests.post(webhook_url, json=discord_hook)
