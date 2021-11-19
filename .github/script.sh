3e9123f0f701e15decd6486a01f10d66569e430f
scatowl <scatowl@qq.com>
2021-11-19 12:35:28 +0800

    .

--- .github/script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for file in $GITHUB_WORKSPACE/jobs/*
-do
-  docker run --rm --pull always --volume "$file:/app/data/device_auths.json" ghcr.io/jackblk/epicgames-freebies-claimer:latest > epic.log
-  python webhook_parser.py
-  curl -H "Content-Type: application/json" -d@webhook.json $Discord_Webhook
-done
